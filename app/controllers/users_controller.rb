7638d3885975757e887c58bcd6c399774bc72afb
dwight <dw5ight@gmail.com>
2011-07-30 00:51:35 -0700

    checkpt, have app looking nice and about to make message boxes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController
   def event_feed
-  	if(!session.has_key?("user_id")) then 
-  		redirect_to "/login" 
-  		return
+  	if(!session.has_key?("user_id")) then
+  		#redirect_to "/login"
+  		#return
   	end
-  	@page_title = "Welcome to Hooqup " + session[:user_name].split(" ")[0] + "!"
+  	@page_title = "Welcome to Hooqup " #+ session[:user_name].split(" ")[0] + "!"
   end
-  
+
   def logout
 		session[:user_id]=nil
 		session[:user_name]=nil
 		redirect_to "/login"
 	end
-	
+
   def event_feed_json
     @user = User.new
     render :template => 'users/event_feed.js.erb'
   end
 end
+
